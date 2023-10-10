cae28317235a935005b114b4accedbf24cf48255
jetzerszeri <jetzers.2@gmail.com>
2023-10-09 20:03:52 -0400

    feat: create the config.js file to reuse it

---
+++ js/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyAXetqmfp3CONcNT5NOr8Ol7WpfoGbXDB4",
+authDomain: "gbygpcs.firebaseapp.com",
+projectId: "gbygpcs",
+storageBucket: "gbygpcs.appspot.com",
+messagingSenderId: "89038175259",
+appId: "1:89038175259:web:4a8bb74924cf7a7072ccbe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
