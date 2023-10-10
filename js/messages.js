0ce6e00726287885ef238f6b8571ec35224ab894
jetzerszeri <jetzers.2@gmail.com>
2023-10-09 20:37:01 -0400

    feat: add a new service to the db (it works)

---
+++ js/messages.js
@@ -0,0 +1,13 @@
+const success = `
+<div class="confirmation">
+<p>El vehículo se ha registrado exitosamente!</p>
+<div>
+    <a href="/" class="btn">Ver listado</a>
+</div>
+</div>
+`
+
+
+export {
+    success
+}
