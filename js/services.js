c305ee86f1852ba6e181f0fdb853b13815004c4a
jetzerszeri <jetzers.2@gmail.com>
2023-10-24 21:28:52 -0400

    renderizando los servicios desde el html

--- js/services.js
+++ js/services.js
@@ -1,10 +1,10 @@
 import  app  from './config.js';
 import { getFirestore, collection, addDoc, getDocs, orderBy, query } from 'https://www.gstatic.com/firebasejs/10.3.0/firebase-firestore.js';
 
-const servicesList = document.querySelector('.servicescontainer section ul');
-const main = document.querySelector('.servicescontainer');
-const sectionServices = document.createElement('section');
-const ulServices = document.createElement('ul');
+// const servicesList = document.querySelector('.servicescontainer section ul');
+// const main = document.querySelector('.servicescontainer');
+// const sectionServices = document.createElement('section');
+// const ulServices = document.createElement('ul');
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
@@ -17,32 +17,32 @@ const q = query(collection(db, "services"), orderBy("name"));
 //     return services;
 // }
 const services = await getDocs(q)
-.then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        let item = document.createElement('li');
-        let divContainer = document.createElement('div');
-        let divImg = document.createElement('div');
-        let img = document.createElement('img');
-        img.src = 'img/servicesimg.jpg';
-        img.alt = doc.data().name;
-        let divInfo = document.createElement('div');
-        divInfo.innerHTML = `
-        <div>
-            <h2>${doc.data().name}</h2>
-            <p>${doc.data().category}</p>
-        </div>
-        <p>$${doc.data().price}/sqft</p>
-        <a href="contact.html" class="btn secundary-green">Contactar</a>`;
-        divImg.append(img);
-        divContainer.append(divImg);
-        divContainer.append(divInfo);
-        item.append(divContainer);
-        ulServices.append(item);
-    });
-});
+// .then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//         let item = document.createElement('li');
+//         let divContainer = document.createElement('div');
+//         let divImg = document.createElement('div');
+//         let img = document.createElement('img');
+//         img.src = 'img/servicesimg.jpg';
+//         img.alt = doc.data().name;
+//         let divInfo = document.createElement('div');
+//         divInfo.innerHTML = `
+//         <div>
+//             <h2>${doc.data().name}</h2>
+//             <p>${doc.data().category}</p>
+//         </div>
+//         <p>$${doc.data().price}/sqft</p>
+//         <a href="contact.html" class="btn secundary-green">Contactar</a>`;
+//         divImg.append(img);
+//         divContainer.append(divImg);
+//         divContainer.append(divInfo);
+//         item.append(divContainer);
+//         ulServices.append(item);
+//     });
+// });
 
-sectionServices.append(ulServices);
-main.appendChild(sectionServices);
+// sectionServices.append(ulServices);
+// main.appendChild(sectionServices);
 
 // const services = await getDocs(q);
 export default services;
