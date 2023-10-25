c50d6892ee6ef3d77a06d3a09ad6572f877cb4a7
jetzerszeri <jetzers.2@gmail.com>
2023-10-24 21:13:59 -0400

    test: si funciona la consulta
    y mostrar cada servicio en el console log

--- js/services.js
+++ js/services.js
@@ -1,42 +1,51 @@
 import  app  from './config.js';
 import { getFirestore, collection, addDoc, getDocs, orderBy, query } from 'https://www.gstatic.com/firebasejs/10.3.0/firebase-firestore.js';
 
-const servicesList = document.querySelector('.servicescontainer section ul');
-const main = document.querySelector('.servicescontainer');
-const sectionServices = document.createElement('section');
-const ulServices = document.createElement('ul');
+// const servicesList = document.querySelector('.servicescontainer section ul');
+// const main = document.querySelector('.servicescontainer');
+// const sectionServices = document.createElement('section');
+// const ulServices = document.createElement('ul');
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
-const servicesRef = collection(db, "services");
-const q = query(servicesRef, orderBy("name"));
+// const servicesRef = collection(db, "services");
+// const q = query(servicesRef, orderBy("name"));
 
+const q = query(collection(db, "services"), orderBy("name"));
 
+// async function fetchServices() {
+//     const services = await getDocs(q);
+//     return services;
+// }
+// const services = await getDocs(q);
+// .then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//         let item = document.createElement('li');
+//         let divContainer = document.createElement('div');
+//         let divImg = document.createElement('div');
+//         let img = document.createElement('img');
+//         img.src = 'img/servicesimg.jpg';
+//         img.alt = doc.data().name;
+//         let divInfo = document.createElement('div');
+//         divInfo.innerHTML = `
+//         <div>
+//             <h2>${doc.data().name}</h2>
+//             <p>${doc.data().category}</p>
+//         </div>
+//         <p>$${doc.data().price}/sqft</p>
+//         <a href="contact.html" class="btn secundary-green">Contactar</a>`;
+//         divImg.append(img);
+//         divContainer.append(divImg);
+//         divContainer.append(divInfo);
+//         item.append(divContainer);
+//         ulServices.append(item);
+//     });
+// });
 
-await getDocs(q).then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        let item = document.createElement('li');
-        let divContainer = document.createElement('div');
-        let divImg = document.createElement('div');
-        let img = document.createElement('img');
-        img.src = 'img/servicesimg.jpg';
-        img.alt = doc.data().name;
-        let divInfo = document.createElement('div');
-        divInfo.innerHTML = `
-        <div>
-            <h2>${doc.data().name}</h2>
-            <p>${doc.data().category}</p>
-        </div>
-        <p>$${doc.data().price}/sqft</p>
-        <a href="contact.html" class="btn secundary-green">Contactar</a>`;
-        divImg.append(img);
-        divContainer.append(divImg);
-        divContainer.append(divInfo);
-        item.append(divContainer);
-        ulServices.append(item);
-    });
-});
+// sectionServices.append(ulServices);
+// main.appendChild(sectionServices);
 
-sectionServices.append(ulServices);
-main.appendChild(sectionServices);
+const services = await getDocs(q);
+export default services;
+
