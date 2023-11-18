d98c2ad594e69f3663e4ff3c0c09cedd1a375a2f
jetzerszeri <jetzers.2@gmail.com>
2023-11-18 16:41:55 -0500

    ejemplo de modularización de vistas

---
+++ js/views/home.js
@@ -0,0 +1,8 @@
+const home = `
+    <div class="home">
+        <h1>home</h1>
+        <p>Here you can find our home</p>
+    </div>
+`;
+
+export default home;
---
+++ js/views/products.js
@@ -0,0 +1,8 @@
+const products = `
+    <div class="products">
+        <h1>Products</h1>
+        <p>Here you can find our products</p>
+    </div>
+`;
+
+export default products;
