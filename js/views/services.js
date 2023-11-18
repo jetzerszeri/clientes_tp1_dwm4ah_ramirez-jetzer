146df4fd4d6777dd3d66e2cb4612cbf3e2ddcb07
jetzerszeri <jetzers.2@gmail.com>
2023-11-18 17:44:03 -0500

    feat: start refactor and render of services view

---
+++ js/views/services.js
@@ -0,0 +1,24 @@
+const servicesView = `
+<main class="servicescontainer">
+    <header class="servicesheader">
+        <div>
+            <h1>Servicios profesionales de limpieza</h1>
+            <p>Elevando el estandar de limpieza con soluciones personalizadas.</p>
+        </div>
+
+        <div>
+            <ul class="filterBtns">
+                <li><a href="" class="btn secundary-green">Remodelación</a></li>
+                <li><a href="" class="btn secundary-green">Mudanza</a></li>
+                <li><a href="" class="btn secundary-green">Real State</a></li>
+            </ul>
+        </div>
+    </header>
+
+
+
+</main>
+
+`;
+
+export default servicesView;
