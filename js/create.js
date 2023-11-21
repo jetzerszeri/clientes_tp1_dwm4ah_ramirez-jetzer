868cf95fcace461208555fe09c7126d9716554bc
jetzerszeri <jetzers.2@gmail.com>
2023-11-21 18:53:52 -0500

    feat: add the textContent option to he create element function

--- js/create.js
+++ js/create.js
@@ -1,9 +1,12 @@
-function element(elementToCreate, elementClassList){
+function element(elementToCreate, elementClassList, textContent){
     let element = document.createElement(elementToCreate);
     if (elementClassList){
         elementClassList.forEach(elementClass => {
             element.classList.add(elementClass);
         });
+    }
+    if (textContent){
+        element.textContent = textContent;
     }
     return element;
 }
